75e0302c12dde7219c2c0a6f6ddc449b23c5cbb0
Lassad KEFI <lass3ouda@gmail.com>
2025-08-10 13:41:19 +0200

    feat: allow filtering municipalities and document usage

--- tests/TunisianMunicipalityTest.php
+++ tests/TunisianMunicipalityTest.php
@@ -6,6 +6,7 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Middleware;
 use PHPUnit\Framework\TestCase;
 use TunisianMunicipality\TunisianMunicipality;
 
@@ -26,5 +27,28 @@ class TunisianMunicipalityTest extends TestCase
 
         $this->assertIsArray($response);
         $this->assertSame('Test', $response[0]['name']);
+    }
+
+    public function test_get_municipalities_with_filters_adds_query_parameters(): void
+    {
+        $container = [];
+        $history = Middleware::history($container);
+
+        $mock = new MockHandler([
+            new Response(200, [], json_encode([]))
+        ]);
+
+        $handlerStack = HandlerStack::create($mock);
+        $handlerStack->push($history);
+
+        $client = new Client(['handler' => $handlerStack]);
+
+        $api = new TunisianMunicipality($client);
+
+        $api->getMunicipalities(['name' => 'Tunis']);
+
+        $this->assertNotEmpty($container);
+        $request = $container[0]['request'];
+        $this->assertSame('name=Tunis', $request->getUri()->getQuery());
     }
 }
